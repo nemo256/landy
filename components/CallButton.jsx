4c3752de376347f8d0b27c462587c946326eb765
Amine Neggazi <neggazimedlamine@gmail.com>
2022-08-26 13:05:11 +0100

    updated call us button!

--- components/CallButton.jsx
+++ components/CallButton.jsx
@@ -1,7 +1,7 @@
 
 const Button = ({ children, ...props }) => {
   return (
-    <button className={props.className ? props.className + " flex btn px-4 bg-accent text-primary text-base font-semibold rounded-full items-center" : "flex btn px-4 bg-accent text-primary text-base font-semibold rounded-full items-center"}>
+    <button className={props.className ? props.className + " flex btn px-4 bg-accent text-primary text-base font-semibold rounded-full items-center" : "flex btn px-4 py-1 bg-accent text-primary text-base font-semibold rounded-full items-center"}>
       {children}
     </button>
   )
