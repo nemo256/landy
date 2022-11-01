516c035954dd5b46055afca673b43d53e93eb183
Amine Neggazi <neggazimedlamine@gmail.com>
2022-11-01 12:49:52 +0100

    fixed typo!

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <section className="center bg-secondary text-primary h-screen snap-center">
       <p className="mb-8 underline">Two Column</p>
-      <div className="flex flex-row justify-between space-x-32 px-16 ">
+      <div className="flex flex-row justify-between space-x-32 px-16">
         <div className="text-center w-full">
           Hero | First Column
         </div>
