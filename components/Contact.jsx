06a3406bdbc6e8a8ede260a013b2fb1362e34784
Amine Neggazi <neggazimedlamine@gmail.com>
2023-10-18 11:50:28 +0100

    fix contact section text colour

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <section id="contact" className="px-4 center text-center text-primary h-screen snap-center">
+    <section id="contact" className="px-4 center text-center text-white h-screen snap-center">
       Contact
     </section>
   )
